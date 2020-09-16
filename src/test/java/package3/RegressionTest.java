ccd00012724d9450517da771658fc37ddb21673a
vishalanerao <vishal.anerao@gmail.com>
2020-09-16 11:38:29 +0530

    Added package3 with Regression classs

---
+++ src/test/java/package3/RegressionTest.java
@@ -0,0 +1,18 @@
+package package3;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+	@Test
+	public void m1() {
+		System.out.println("I am Fisrt Regression Test");
+	}
+	@Test
+	public void m2() {
+		System.out.println("I am Second Regression Test");
+	}
+	@Test
+	public void m3() {
+		System.out.println("I am Third Regression Test");
+	}
+}
