c87fb135106bdcf1c9e79e5d4569ff8b09ebcc35
vishalanerao <vishal.anerao@gmail.com>
2020-09-24 19:37:03 +0530

    comment added in package3 in regression test

--- src/test/java/package3/RegressionTest.java
+++ src/test/java/package3/RegressionTest.java
@@ -3,6 +3,12 @@ package package3;
 import org.testng.annotations.Test;
 
 public class RegressionTest {
+
+/*
+This is regression test class 
+ there are three test cases which are m1, m2, and m3.
+
+*/
 	@Test
 	public void m1() {
 		System.out.println("I am Fisrt Regression Test");
